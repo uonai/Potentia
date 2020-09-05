81017fe142936755d653b76982bd2583327df9a1
Uonai <cs.mccaleb@gmail.com>
2020-09-05 15:36:36 -0500

    Exception handling.

--- Potentia/Controllers/MaxLiftController.cs
+++ Potentia/Controllers/MaxLiftController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Schema;
@@ -11,12 +13,11 @@ using PotentiaLibrary;
 
 namespace Potentia.Controllers
 {
-
-    [Route("api/[controller]")]
+  
     [ApiController]
     public class MaxLiftController : ControllerBase
     {
-        
+       [Route("api/[controller]")]
         [HttpPost]
         public object Post(UserInfo userInfo)
         {
@@ -24,7 +25,17 @@ namespace Potentia.Controllers
             decimal calculatedKilograms;
             decimal calculatedPounds;
 
-            if (userInfo.Formula == "brzycki")
+            if (userInfo.Formula == "" || userInfo.Metric == "" || userInfo.Repetitions == 0 || userInfo.Weight == 0)
+            {
+                throw new ArgumentException("Provide string values for Formula and Weight. Provide nonzero values for Repetitions and Weight");
+            }
+
+            else if (userInfo.Metric != "kilograms" && userInfo.Metric != "pounds")
+            {
+                throw new ArgumentException("Provide string values for Weight (kilograms or pounds)");
+            }
+
+            else if (userInfo.Formula == "brzycki")
             {
                 calculatedKilograms = Calculations.BrzyckiFormula(userInfo.Repetitions, calculatedMetrics.Kilograms);
                 calculatedPounds = Calculations.BrzyckiFormula(userInfo.Repetitions, calculatedMetrics.Pounds);
@@ -49,7 +60,7 @@ namespace Potentia.Controllers
                 calculatedKilograms = Calculations.OConnerFormula(userInfo.Repetitions, calculatedMetrics.Kilograms);
                 calculatedPounds = Calculations.OConnerFormula(userInfo.Repetitions, calculatedMetrics.Pounds);
             }
-            else { calculatedKilograms = 0; calculatedPounds = 0; }
+            else { throw new ArgumentException("Provide a formula (brzycki, epley, lander, lombardi, oconner)."); }
             return new Lift
             {
                 Kilograms = calculatedKilograms,
